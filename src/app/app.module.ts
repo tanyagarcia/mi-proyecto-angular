f4fef5787975c52f1aaf40fd10b91fb94a23dccc
Tanya García <garcia.tanya0204@gmail.com>
2021-03-20 18:22:16 -0300

    Primer commit. Modificaciones para deploy heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    QuienSoyComponent
   ],
   imports: [
     BrowserModule,
