a85b2280085e2a39c588cce14b21bcf0762be419
Tanya García <garcia.tanya0204@gmail.com>
2021-03-24 17:05:39 -0300

    Cambio favicon, navegación y quién soy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'quien-soy', component: QuienSoyComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
